217e13e3df6d799e17832718ea177714064506ac
Inigo <inigoiparraguirre@gmail.com>
2020-06-04 16:57:30 +0200

    update

--- src/app/download.service.ts
+++ src/app/download.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class DownloadService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  download(url: string): Observable<HttpEvent<Blob>> {
+    return this.http.get(url, {
+      responseType: 'blob',
+      observe: 'events',
+      reportProgress: true
+    });
+  }
 }
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,10 @@
+import { DownloadService } from './../download.service';
 import { UploadService } from './../upload.service';
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-home',
@@ -12,18 +14,24 @@ import { of } from 'rxjs';
 export class HomePage {
 
   @ViewChild('fileUpload', {static: false}) fileUpload: ElementRef;
-  files  = [];
-  progress = 0;
-  loaded = false;
-  url: string;
-  constructor(private uploadService: UploadService) {}
+  filesCert  = [];
+  progressCert = 0;
+  progressDownCert = 0;
+  loadedCert = false;
+  urlCert: string;
+  nameCert: string;
+  constructor(
+    private downloadService: DownloadService,
+    private uploadService: UploadService) {}
 
-  onClick() {
+  
+
+  onClickCert() {
     const fileUpload = this.fileUpload.nativeElement;
     fileUpload.onchange = () => {
     for (let index = 0; index < fileUpload.files.length; index++) {
      const file = fileUpload.files[index];
-     this.files.push({ data: file, inProgress: false, progress: 0});
+     this.filesCert.push({ data: file, inProgress: false, progress: 0});
     }
 
     this.uploadFiles();
@@ -32,16 +40,17 @@ export class HomePage {
     fileUpload.click();
   }
 
-  getProgress(): number {
-    return this.progress;
+  getProgressCert(): number {
+    return this.progressCert;
   }
 
-  setProgress(value: number) {
-    this.progress = value;
+  setProgressCert(value: number) {
+    this.progressCert = value;
   }
 
   uploadFile(file) {
     const formData = new FormData();
+    this.progressDownCert = 0;
     formData.append('file', file.data);
     file.inProgress = true;
     this.uploadService.upload(formData).pipe(
@@ -49,8 +58,8 @@ export class HomePage {
         switch (event.type) {
           case HttpEventType.UploadProgress:
             file.progress = Math.round(event.loaded * 100 / event.total);
-            console.log('progress value', this.progress, event);
-            this.progress = file.progress;
+            console.log('progress value', this.progressCert, event);
+            this.setProgressCert(file.progress);
             break;
           case HttpEventType.Response:
             console.log('evento response');
@@ -62,22 +71,64 @@ export class HomePage {
         return of(`${file.data.name} upload failed.`);
       })).subscribe((event: any) => {
         if (typeof (event) === 'object') {
-          this.loaded = true;
-          this.url = event.body.link;
-          console.log(this.url);
+          this.loadedCert = true;
+          this.urlCert = event.body.link;
+          this.nameCert = file.data.name;
+          console.log(this.urlCert);
         }
       });
   }
 
   private uploadFiles() {
     this.fileUpload.nativeElement.value = '';
-    this.files.forEach(file => {
-      console.log("Cargar fichero");
+    this.filesCert.forEach(file => {
+      console.log('Cargar fichero');
       this.uploadFile(file);
     });
 
-    this.files = [];
-}
+    this.filesCert = [];
+  }
 
+  downloadCert() {
+    this.downloadService
+      .download(this.urlCert)
+      .pipe(
+        map(event => {
+          switch (event.type) {
+            case HttpEventType.Sent:
+              break;
+            case HttpEventType.DownloadProgress:
+              // tslint:disable-next-line: variable-name
+              const progressDown = Math.round(event.loaded * 100 / event.total);
+              console.log('progress download value', event.loaded, progressDown);
+              return progressDown;
+              break;
+            case HttpEventType.Response:
+              console.log('evento response');
+              return event.body;
+          }
+        }),
+      )
+      .subscribe(data => {
+        console.log('Descarga');
+        if (typeof (data) === 'number') {
+          this.progressDownCert = data;
+        }
+
+        if (typeof (data) === 'object') {
+          saveAs(data, 'upv-descarga');
+          this.progressDownCert = 1;
+        }
+
+      }
+        );
+  }
+
+  removeCert() {
+    this.loadedCert = false;
+    this.setProgressCert(0);
+    this.progressDownCert = 0;
+    this.filesCert = [];
+  }
 
 }
